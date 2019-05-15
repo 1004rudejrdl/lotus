7b0dcaa4f683ad778dec5255cfcd97c423680344
LiBReShiNn <shinhj1991@gmail.com>
2019-05-16 04:04:57 +0900

    sub megamenu 수정

--- lib/mega_menu_sub.php
+++ lib/mega_menu_sub.php
@@ -1,9 +1,9 @@
 <?php  ?>
 <div class="row">
-  <div class="column">
+  <div class="column longer">
     <h3>이성찾기</h3>
-    <a href="../about_company/brandstory.php">남</a>
-    <a href="../about_company/ceo.php">여</a>
+    <a href="../about_company/brandstory.php" style="color:#1565c0">남</a>
+    <a href="../about_company/ceo.php" style="color:#f64f59">여</a>
     <a href="../about_company/com_location.php">데이트로그/회원현황</a>
     <a href="../about_company/history.php">이상형 설문조사</a>
   </div> <!-- column -->
@@ -11,15 +11,15 @@
     <h3>추천/예약</h3>
     <a href="../about_chain/chain_intro.php">맛집</a>
     <a href="../about_chain/chain_inquiry.php">숙박</a>
-    <a href="../about_chain/chain_apply.php">렌트카</a>
+    <a href="../ra_car_ticket/ra_category.php">렌트카</a>
   </div> <!-- column -->
   <div class="column">
     <h3>쇼핑몰</h3>
     <a href="../sh_man/sh_man_list.php?mode=man">남성의류</a>
-    <a href="../sh_man/sh_man_list.php?mode=woman">여성의류 </a>
+    <a href="../sh_man/sh_man_list.php?mode=woman">여성의류</a>
     <a href="../sh_man/sh_man_list.php?mode=shose">신발</a>
   </div> <!-- column -->
-  <div class="column">
+  <div class="column longer">
     <h3>테스트</h3>
     <a href="../about_com_notice/com_ann.php">연애진단</a>
     <a href="../about_com_notice/com_event.php">연애성향테스트</a>
