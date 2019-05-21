7c3d50a828e95fc7b05041cf8938fa1a6466661b
1004rudejrdl <1004rudejrdl@naver.com>
2019-05-21 15:06:50 +0900

    5/21 중간 파일 업로드

--- sh_man/shop_register.php
+++ sh_man/shop_register.php
@@ -6,6 +6,19 @@
   // include $_SERVER['DOCUMENT_ROOT']."/lotus/lib/create_table.php";
   // include $_SERVER['DOCUMENT_ROOT']."/lotus/lib/func_main.php";
   // include __DIR__."/../lib/create_table.php"; 자기 폴더 까지 찍으므로 상대경로의 문제점을 고치지는 못함
+
+  if($_GET['mode']=="man"){
+    $name="남성패션";
+    $list_name="man";
+  }else if($_GET['mode']=="woman"){
+    $name="여성패션";
+    $list_name="woman";
+  }else if($_GET['mode']=="shose"){
+    $name="신발";
+    $list_name="shose";
+  }
+
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,10 +29,13 @@
   <link rel="stylesheet" href="../css/common.css">
   <!-- <link rel="stylesheet" href="../css/join.css"> -->
   <link rel="stylesheet" href="../css/header_sidenav.css">
+  <link rel="stylesheet" href="./css/list.css">
   <!-- <script type="text/javascript" src="../js/sign_update_check_html.js?ver=1" ></script> -->
   <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
   <!-- <script type="text/javascript" src="../js/sign_update_check_ajax_main.js?ver=1"></script> -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
+
 <body>
 <!-- header start -->
   <?php include $_SERVER['DOCUMENT_ROOT']."/lotus/lib/header_sidenav.php"; ?>
@@ -33,60 +49,44 @@
   <a href="./sh_man_list.php?mode=shose">신발</a>
 </div><!-- sidenav end -->
 <div class="main">
+  <div class="">
+    <form class="" action="./shop_register_insert.php?mode=<?=$list_name?>" method="post" enctype="multipart/form-data">
 
-<div class="">
-
-
-<h1>장바구니</h1>
-<div class="">
-  <div class="" style="width:100%; text-align:center;background-color:#dddddd">
-    <h3 >일반 구매</h3>
-  </div>
-  <table border="1px" style="width:100%">
+  <table style="width:100%">
     <tr>
-      <td>1</td>
-      <td>1</td>
-      <td>1</td>
-      <td>1</td>
+      <td>회사 이름</td>
+      <td> <input type="text" name="company_name" value=""> </td>
+      <td>샾 이름 </td>
+      <td> <input type="text" name="shop_name" value=""> </td>
     </tr>
     <tr>
-      <td colspan="4">1</td>
+      <td>샾 이미지</td>
+      <td> <input type="file" name="logo_img" value=""> </td>
+      <td>샾 링크</td>
+      <td> <input type="text" name="shop_link" value=""></td>
     </tr>
     <tr>
-      <td rowspan="2">1</td>
-      <td>1</td>
-      <td rowspan="2">1</td>
-      <td rowspan="2">1</td>
+      <td>문의번호</td>
+      <td><input type="text" name="shop_tel" value=""> </td>
+      <td>주소</td>
+      <td>
+        <button type="button" name="button">주소 등록하기</button> <br>
+        <input type="text" name="shop_addr1" value=""><br>
+        <input type="text" name="shop_addr2" value="">
+      </td>
     </tr>
     <tr>
-      <td>1</td>
+      <td >유의사항</td>
 
-    </tr>
-    <tr>
-      <td colspan="4">1</td>
+      <td colspan="3"> <textarea name="shop_note" rows="6" cols="60"></textarea> </td>
+
     </tr>
   </table>
-  <input type="checkbox" name="" value="">전체 선택
-  <button type="button" name="button">전체 삭제</button>
-  <button type="button" name="button">품절/판매종료 상품 전체 삭제</button>
-  <fieldset style="text-align:center">
-    총 상품가격 50000원 = 총 주문 금액 50000원
-  </fieldset>
+  <div class="" style="text-align:center">
+    <input type="submit" name="" value="등록하기">
+  </div>
+</form>
 </div>
-
-<div class="" style="text-align:center">
-  <button type="button" name="button" onclick="history.go(-1)">계속 쇼핑하기</button>
-  <a href="./shopping_payment.php"> <button type="button" name="button">구매하기</button></a>
-</div>
-
-
-
-
-
-</div>
-
-
-
 </div>  <!-- main end -->
 </div>  <!-- main_body end -->
 <!-- footer start -->
