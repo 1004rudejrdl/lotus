0559ef96338b3a65cbba2726a9718ef006dffdff
LiBReShiNn <shinhj1991@gmail.com>
2019-05-28 09:36:08 +0900

    데이트로그 파일경로 오류 재수정

--- find_meet/match_log.php
+++ find_meet/match_log.php
@@ -21,62 +21,73 @@ $number = $total_record - $start;
 ?>
 <!DOCTYPE html>
 <html lang="ko" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../css/common.css">
-    <link rel="stylesheet" href="../css/main.css">
-    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
-    <link rel="stylesheet" href="../css/header_sidenav.css">
-    <title></title>
-  </head>
-  <body>
-    <?php include $_SERVER['DOCUMENT_ROOT']."/lotus/lib/header_sidenav.php"; ?>
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../css/common.css">
+  <link rel="stylesheet" href="./css/match_log.css">
+  <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+  <link rel="stylesheet" href="../css/header_sidenav.css">
+  <title></title>
+</head>
+
+<body>
+  <?php include $_SERVER['DOCUMENT_ROOT']."/lotus/lib/header_sidenav.php"; ?>
   <!-- header end -->
   <!-- main_body start -->
   <div class="main_body">
-  <div id="sidenav" class="sidenav">
-    <a href="#about">추천/예약</a>
-    <a href="#services">맛집</a>
-    <a href="#clients">숙박</a>
-    <a href="#contact">렌트카</a>
-  </div><!-- sidenav end -->
-  <div class="main">
-    <div class="br_st_text">
-      <div class="timeline">
-    <?php
-     for ($i = $start; $i < $start+SCALE && $i<$total_record; $i++){
-       mysqli_data_seek($result,$i);
-       $row=mysqli_fetch_array($result);
-       $id=$row['id'];
-       $matching=$row['matching'];
-       $matching_day=$row['matching_day'];
-       if ($left_right=="left") {
-         $left_right="right";
-       }else{
-         $left_right="left";
-       }
-       ?>
-              <div class="container <?=$left_right?>">
-                <div class="content">
-                  <h2><?= $matching_day?></h2>
-                  <p><?=$id?> ♥ <?=$matching?></p>
-                </div>
-              </div>
-
-          <?php
-
-          $number--;
-
-        }//end of for
-        ?>
-
+    <div id="sidenav" class="sidenav">
+      <a href="#about">추천/예약</a>
+      <a href="#services">맛집</a>
+      <a href="#clients">숙박</a>
+      <a href="#contact">렌트카</a>
+    </div><!-- sidenav end -->
+    <div class="main">
+      <div class="admin_title">
+        데이트 로그
       </div>
-    </div>
-
-  </div>  <!-- main end -->
-  </div>  <!-- main_body end -->
+      <hr class="title_hr">
+      <div class="admin_title_right">
+        현재 까지 연,꽃을 통해 인연을 찾으신 회원님들 입니다
+      </div>
+      <div class="log_img">
+        <table>
+          <img src="./img/log_match.jpg" alt="">
+        </table>
+      </div>
+      <div class="log_text">
+        <div class="timeline">
+          <?php
+           for ($i = $start; $i < $start+SCALE && $i<$total_record; $i++){
+           mysqli_data_seek($result,$i);
+           $row=mysqli_fetch_array($result);
+           $id=$row['id'];
+           $matching=$row['matching'];
+           $matching_day=$row['matching_day'];
+           //좌우 번갈아 가면서 찍도록하는 플래그
+           if ($left_right=="left") {
+             $left_right="right";
+           }else{
+             $left_right="left";
+           }
+           ?>
+            <div class="container <?=$left_right?>">
+              <div class="content">
+                <p><b><?= $matching_day?></b></p>
+                <p><?=$id?> ♥ <?=$matching?></p>
+              </div>
+            </div>
+            <?php
+            $number--;
+            }//end of for
+            ?>
+        </div><!-- timeline end -->
+      </div><!-- log_text end -->
+    </div> <!-- main end -->
+  </div> <!-- main_body end -->
   <!-- footer start -->
   <?php include $_SERVER['DOCUMENT_ROOT']."/lotus/lib/footer.php"; ?>
-  </body>
+</body>
+
 </html>
